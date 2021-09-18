384e71d134f552d45402153889408b6f1c436197
Shen-hub <57696435+Shen-hub@users.noreply.github.com>
2021-09-18 14:22:37 +0800

    Update mover.lua

--- circle/mover.lua
+++ circle/mover.lua
@@ -6,9 +6,15 @@ function Mover:create(x,y)
 	setmetatable(mover, Mover)
 	mover.location = location
 	mover.velocity = velocity
+	mover.acceleration = Vector:create(0.0)
 	return mover
 end
 
 function Mover:draw()
 	love.graphics.circle("fill", self.location.x, self.location.y, 20)
+end
+
+function  Mover:update()
+	self.velocity = self.velocity + self.acceleration
+	self.location = self.location + self.velocity
 end
